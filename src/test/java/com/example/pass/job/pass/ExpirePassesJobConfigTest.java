f952e537f6c55cc89553f7f9ae18b263384e44e9
qnddjKJH.dev <qnddjKJH.dev@gmail.com>
2022-10-18 22:19:43 +0900

    Add Batch expirePasses & Batch test

---
+++ src/test/java/com/example/pass/job/pass/ExpirePassesJobConfigTest.java
@@ -0,0 +1,69 @@
+package com.example.pass.job.pass;
+
+import com.example.pass.job.pass.config.TestBatchConfig;
+import com.example.pass.repository.pass.PassEntity;
+import com.example.pass.repository.pass.PassRepository;
+import com.example.pass.repository.pass.PassStatus;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.batch.core.ExitStatus;
+import org.springframework.batch.core.JobExecution;
+import org.springframework.batch.core.JobInstance;
+import org.springframework.batch.test.JobLauncherTestUtils;
+import org.springframework.batch.test.context.SpringBatchTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@Slf4j
+@SpringBootTest
+@SpringBatchTest
+@ActiveProfiles("test")
+@ContextConfiguration(classes = {ExpirePassesJobConfig.class, TestBatchConfig.class})
+class ExpirePassesJobConfigTest {
+
+    @Autowired
+    private JobLauncherTestUtils jobLauncherTestUtils;
+    @Autowired
+    private PassRepository passRepository;
+
+    @Test
+    public void test_expirePassesStep() throws Exception {
+        addPassEntities(10);
+
+        JobExecution jobExecution = jobLauncherTestUtils.launchJob();
+        JobInstance jobInstance = jobExecution.getJobInstance();
+
+        assertThat(jobExecution.getExitStatus()).isEqualTo(ExitStatus.COMPLETED);
+        assertThat(jobInstance.getJobName()).isEqualTo("expirePassesJob");
+    }
+
+    private void addPassEntities(int size) {
+        final LocalDateTime now = LocalDateTime.now();
+        final Random random = new Random();
+
+        List<PassEntity> passEntities = new ArrayList<>();
+        for (int i = 0; i < size; i++) {
+            passEntities.add(
+                    PassEntity.of(
+                            1L,
+                            "A" + 1000000 + i,
+                            PassStatus.PROGRESSED,
+                            random.nextLong(11),
+                            now.minusDays(60),
+                            now.minusDays(1)
+                    )
+            );
+        }
+        passRepository.saveAll(passEntities);
+    }
+
+}
---
+++ src/test/java/com/example/pass/job/pass/config/TestBatchConfig.java
@@ -0,0 +1,19 @@
+package com.example.pass.job.pass.config;
+
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@Configuration
+@EnableJpaAuditing
+@EnableAutoConfiguration
+@EnableBatchProcessing
+@EntityScan("com.example.pass.repository")
+@EnableJpaRepositories("com.example.pass.repository")
+@EnableTransactionManagement
+public class TestBatchConfig {
+}
